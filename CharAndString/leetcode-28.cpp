75d0631f322294fda3de7c10e03321f8a60d4969
wangyu1092 <wangyu1092@163.com>
2020-09-19 22:53:05 +0800

    Leetcode-28 字符串匹配-KMP

--- CharAndString/leetcode-28.cpp
+++ CharAndString/leetcode-28.cpp
@@ -1,5 +1,16 @@
 
-// https://leetcode-cn.com/problems/implement-strstr/
+// 给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
+
+// 示例 1:
+// 输入: haystack = "hello", needle = "ll"
+// 输出: 2
+
+// 示例 2:
+// 输入: haystack = "aaaaa", needle = "bba"
+// 输出: -1
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/implement-strstr
 
 class Solution {
 public:
